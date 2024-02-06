742876b2ece3180d5898e4e75367fc306da11c42
Samuel Maina <samuelmayna@gmail.com>
2024-02-06 15:37:35 +0300

    feat: Add admin entity and its tests

--- src/main/java/com/emtech/ushurusmart/usermanagement/model/Admin.java
+++ src/main/java/com/emtech/ushurusmart/usermanagement/model/Admin.java
@@ -6,48 +6,33 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+
+import com.emtech.ushurusmart.payment.model.PaymentDetails;
+
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
-@Entity
-@Table(
-        uniqueConstraints = {
-                @UniqueConstraint(name="kra_unique",columnNames = "KRAPin")
-        }
-)
-public class Admin {
-    @Id
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "admin_sequence"
-    )
-    @Column(
-            nullable = false
-    )
-    private Long id;
-    @Column(
-            nullable = false
-    )
-    private String KRAPin;
-    @Column(
-            nullable = false
-    )
-    private String businessName;
-    @Column(
-            columnDefinition = "TEXT",
-            nullable = false
-    )
-    private String fullName;
-    @Column(
-            nullable = false
-    )
-    private String password;
-    @Column(
-            nullable = false
-    )
-    private Integer phoneNumber;
-    @OneToMany(mappedBy = "admin",cascade = CascadeType.ALL)
-    private List<Users> usersList;
 
+@Entity
+@Table(uniqueConstraints = {
+                @UniqueConstraint(name = "kra_unique", columnNames = "KRAPin")
+})
+public class Admin {
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "admin_sequence")
+        @Column(nullable = false)
+        private Long id;
+        @Column(nullable = false)
+        private String KRAPin;
+        @Column(nullable = false)
+        private String username;
+        @Column(nullable = false)
+        private String password;
+
+        @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL)
+        private List<Users> usersList;
+
+        @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL)
+        private List<PaymentDetails> payments;
 
 }
