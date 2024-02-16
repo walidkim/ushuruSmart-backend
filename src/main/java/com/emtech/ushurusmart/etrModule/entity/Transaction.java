e6bff80c3198b5806c6aa0ca08b0f87be1fe0892
NorbatFrank <norbatfrank@gmail.com>
2024-02-16 12:16:57 +0300

    Json parsing new

---
+++ src/main/java/com/emtech/ushurusmart/etrModule/entity/Transaction.java
@@ -0,0 +1,21 @@
+package com.emtech.ushurusmart.etrModule.entity;
+
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    private double invoiceNumber;
+    private double amount;
+    private LocalDateTime date;
+    private String buyerPin;
+}
--- src/main/java/com/emtech/ushurusmart/etrModule/service/etimsRequests.java
+++ src/main/java/com/emtech/ushurusmart/etrModule/service/etimsRequests.java
@@ -6,9 +6,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.emtech.ushurusmart.etrModule.entity.EtrJackson;
+import com.emtech.ushurusmart.etrModule.entity.Transaction;
+
 @Service
 public class etimsRequests {
     private final String kra_endpoint= "https://etims-api-sbx.kra.go.ke";
+    Transaction trans= EtrJackson.parseJson(json"");
+    if(trans != null){
+        trans.getBuyerPin();
+    }
 
 
 }
