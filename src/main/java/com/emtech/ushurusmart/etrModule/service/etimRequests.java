131ac2b9bea119dd40c31b7ede22e03d752d3eea
brianmuli <brianmulimuteti@gmail.com>
2024-02-16 08:26:45 +0300

    backend branch(1)

--- src/main/java/com/emtech/ushurusmart/etrModule/service/etimRequests.java
+++
@@ -1,9 +0,0 @@
-package com.emtech.ushurusmart.etrModule.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class etimRequests {
-    private final String kra_endpoint= "https://etims-api-sbx.kra.go.ke";
-    
-}
