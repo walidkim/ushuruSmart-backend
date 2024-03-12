1133600e64b5ac628edb5c4fc192838d7747f16f
NorbatFrank <norbatfrank@gmail.com>
2024-03-12 09:01:57 +0300

    TRansaction Crud

--- ushuru-smart-backend-main/src/main/java/com/emtech/ushurusmart/etrModule/repository/TransactionRepository.java
+++ ushuru-smart-backend-main/src/main/java/com/emtech/ushurusmart/etrModule/repository/TransactionRepository.java
@@ -1,10 +1,2 @@
-package com.emtech.ushurusmart.etrModule.repository;
-
-import com.emtech.ushurusmart.etrModule.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+package com.emtech.ushurusmart.etrModule.repository;public interface TransactionRepository {
 }
-
--- ushuru-smart-backend-main/src/main/java/com/emtech/ushurusmart/etrModule/service/TransactionService.java
+++ ushuru-smart-backend-main/src/main/java/com/emtech/ushurusmart/etrModule/service/TransactionService.java
@@ -1,10 +1,2 @@
-package com.emtech.ushurusmart.etrModule.service;
-import com.emtech.ushurusmart.etrModule.repository.TransactionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class TransactionService {
-    private TransactionRepository transactionRepository;
-
+package com.emtech.ushurusmart.etrModule.service;public class TransactionService {
 }
