25952e82c0d591debe1a66317cbd2633a953b2a9
Samuel Maina <samuelmayna@gmail.com>
2024-03-18 12:29:13 +0300

    Resolved merge conflicts

--- ushuru-smart-backend-main/src/test/java/com/emtech/ushurusmart/repository/ProductsRepositoryTest.java
+++ ushuru-smart-backend-main/src/test/java/com/emtech/ushurusmart/repository/ProductsRepositoryTest.java
@@ -1,3 +1,28 @@
 package com.emtech.ushurusmart.repository;
+
+import com.emtech.ushurusmart.etrModule.repository.ProductsRepository;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.emtech.ushurusmart.etrModule.entity.Products;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@SpringBootTest
 public class ProductsRepositoryTest {
+    @Autowired
+    private ProductsRepository productsRepository;
+    @Test
+    public void saveMethod(){
+        Products products = new Products();
+
+        products.setName("Maize");
+        products.setDescription("white, whole grain");
+        products.setQuantity(90);
+        products.setTaxExempted(false);
+        products.setUnitOfMeasure("kg");
+        products.setUnitPrice(4000);
+
+        Products savedObject=productsRepository.save(products);
+    }
+
 }
