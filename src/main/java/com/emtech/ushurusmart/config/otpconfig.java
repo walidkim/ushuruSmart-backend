e2c46845ebc146c3fcb806585b1ae438c8c38187
Walid Kimaiyo <68472615+walidkim@users.noreply.github.com>
2024-04-16 12:03:12 +0300

    Update otpconfig.java

--- src/main/java/com/emtech/ushurusmart/config/otpconfig.java
+++ src/main/java/com/emtech/ushurusmart/config/otpconfig.java
@@ -7,7 +7,6 @@ import lombok.Data;
 @Data
 @Configuration
 public class otpconfig {
-
     // twilio configuration properties
     public static final String ACCOUNT_SID = "ACc26c8aa04fa5255e88000124087eaee0";
     public static final String AUTH_TOKEN = "f7f93b9cb4b4aff3a790a1803b076cf5";
