473d6287ffeb1b55ce07210aa22cf5f44e27147a
Samuel Maina <samuelmayna@gmail.com>
2024-04-24 23:42:15 +0300

    chore: saving progress.

--- src/test/java/com/emtech/ushurusmart/usermanagement/service/OwnerServiceTest.java
+++ src/test/java/com/emtech/ushurusmart/usermanagement/service/OwnerServiceTest.java
@@ -1,10 +1,16 @@
 package com.emtech.ushurusmart.usermanagement.service;
 
 import com.emtech.ushurusmart.etims_middleware.EtimsMiddleware;
+import com.emtech.ushurusmart.usermanagement.Dtos.LoginRequest;
 import com.emtech.ushurusmart.usermanagement.Dtos.OwnerDto;
+import com.emtech.ushurusmart.usermanagement.controller.Utils;
 import com.emtech.ushurusmart.usermanagement.model.Owner;
+import com.emtech.ushurusmart.usermanagement.model.Role;
 import com.emtech.ushurusmart.usermanagement.repository.OwnerRepository;
 import com.emtech.ushurusmart.utils.controller.ResContructor;
+import com.emtech.ushurusmart.utils.otp.OTPService;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -12,8 +18,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Collections;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,6 +41,14 @@ class OwnerServiceTest {
 
     @Mock
     private PasswordEncoder passwordEncoder;
+
+
+    @Mock
+    private OTPService otpService;
+
+
+    @Mock
+    private AuthenticationManager authenticationManager;
 
     @InjectMocks
     private OwnerService userService; // Assuming your validateAndCreateUser method is in a UserService class
@@ -90,6 +107,57 @@ class OwnerServiceTest {
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
         verify(userRepository, times(1)).save(any(Owner.class));
         assertTrue(Objects.requireNonNull(response.getBody()).getMessage().contains("Owner created successfully!"));
+    }
+
+    @Test
+    void testLoginOwner_Success() throws Exception {
+        // Arrange
+        String email = "test@example.com";
+        String password = "password";
+        String type = "owner";
+        LoginRequest loginReq = new LoginRequest(email, password);
+        ResContructor res = new ResContructor();
+        Owner owner = new Owner();
+        owner.setEmail(email);
+        owner.setPhoneNumber("1234567890");
+
+        owner.setRole(Role.owner);
+        when(userService.findByEmail(email)).thenReturn(owner);
+        when(authenticationManager.authenticate(any()))
+                .thenReturn(new UsernamePasswordAuthenticationToken(email, password, Collections.emptyList()));
+
+        // Act
+        ResponseEntity<ResContructor> response = userService.loginOwner(type, loginReq, res);
+        LoginRes parsed= Utils.parseJsonString(Objects.requireNonNull(response.getBody().()), LoginRes.class);
+        // Assert
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(Objects.requireNonNull(response.getBody()).getMessage(),("A short code has been sent to your phone for verification"));
+        assert parsed != null;
+        assertEquals( parsed.getData().getType(), type);
+        assertEquals( parsed.getData().getPhoneNumber(), "1234567890");
+
+  }
+
+
+
+
+    @Data
+    public static class LoginRes {
+
+        @JsonProperty("message")
+        private String message;
+
+        @JsonProperty("Data")
+        private LoginDetails data;
+
+        @Data
+        public static class LoginDetails {
+            @JsonProperty("phoneNumber")
+            private String phoneNumber;
+
+            @JsonProperty("type")
+            private String type;
+        }
     }
 
 
