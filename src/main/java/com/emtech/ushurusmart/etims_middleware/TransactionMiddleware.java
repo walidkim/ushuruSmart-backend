ff4dd4f02ab2731480992d04e2103af5f5b2f44c
Samuel Maina <samuelmayna@gmail.com>
2024-05-15 11:22:19 +0300

    fix: Restored to working state by commenting out erroneous code.

--- src/main/java/com/emtech/ushurusmart/etims_middleware/TransactionMiddleware.java
+++ src/main/java/com/emtech/ushurusmart/etims_middleware/TransactionMiddleware.java
@@ -1,11 +1,15 @@
 package com.emtech.ushurusmart.etims_middleware;
 
+import com.emtech.ushurusmart.utils.controller.ResContructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 
 @Component
@@ -20,6 +24,26 @@ public class TransactionMiddleware {
                 .uri("/api/v1/etims/tax/make-transaction")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(data)
+                .exchange()
+                .block(); // Block to wait for the response
+
+        if (clientResponse != null) {
+            // Convert the ClientResponse to a ResponseEntity
+            String body = clientResponse.bodyToMono(String.class).block();
+            return ResponseEntity.status(clientResponse.statusCode())
+                    .body(body);
+        } else {
+            // Handle the case where the response is null (e.g., due to a timeout)
+            return ResponseEntity.status(500).body("Error: No response received");
+        }
+    }
+
+    public  ResponseEntity<?> getRangeTransactions(LocalDate startDate, LocalDate endDate){
+
+        WebClient webClient = WebClient.create("http://localhost:" + serverPort);
+
+        ClientResponse clientResponse = webClient.get()
+                .uri("/api/v1/etims/tax/make-range?startDate=%s&endDate=%s", startDate, endDate)
                 .exchange()
                 .block(); // Block to wait for the response
 
