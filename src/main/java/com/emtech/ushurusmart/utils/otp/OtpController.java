fe2a85d4d150e5596bd7da44f7c747bc6eb22a7b
walidkim <kimaiyowalid@gmail.com>
2024-05-27 18:00:37 +0300

    Revert "feat: OTP code sent via email"
    This reverts commit e5458bf74b3124e0e7ada771cdb4c8fe0841cd3e.

--- src/main/java/com/emtech/ushurusmart/utils/otp/OtpController.java
+++ src/main/java/com/emtech/ushurusmart/utils/otp/OtpController.java
@@ -1,33 +1,32 @@
-// package com.emtech.ushurusmart.utils.otp;
+//package com.emtech.ushurusmart.utils.otp;
 //
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 //
-// @RestController
-// @RequestMapping("/api/v1/otp")
-// public class OtpController {
+//@RestController
+//@RequestMapping("/api/v1/otp")
+//public class OtpController {
 //
-// @Autowired
-// OTPService otpService;
+//    @Autowired
+//    OTPService otpService;
 //
 //
-//// @GetMapping("/genotp")
-//// public String generateOTP() {
-//// return otpservice.generateOTP();
-//// }
+////    @GetMapping("/genotp")
+////    public String generateOTP() {
+////        return otpservice.generateOTP();
+////    }
 //
-// @GetMapping("/sendotp")
-// public String sendotp(@RequestBody OtpEntity sendOTPRequest) {
+//    @GetMapping("/sendotp")
+//    public String sendotp(@RequestBody OtpEntity sendOTPRequest) {
 //
-// return otpService.sendOTP(sendOTPRequest.getUserTag());
-// }
+//        return otpService.sendOTP(sendOTPRequest.getUserTag());
+//    }
 //
-// @GetMapping(path = "/verifyotp")
-// public boolean verifyOTP(@RequestBody OtpEntity verifyOTPRequest) {
-// return otpService.verifyOTP(verifyOTPRequest.getUserTag(),
-// verifyOTPRequest.getOtpCode());
-// }
-// }
+//    @GetMapping(path = "/verifyotp")
+//    public boolean verifyOTP(@RequestBody OtpEntity verifyOTPRequest) {
+//        return otpService.verifyOTP(verifyOTPRequest.getUserTag(), verifyOTPRequest.getOtpCode());
+//    }
+//}
